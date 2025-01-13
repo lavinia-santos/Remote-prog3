824cbf24c95fcea9a0b701c58deaeda2e0911e17
lavinia-santos <laviniagtsantos15@gmail.com>
2025-01-13 21:40:13 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import util_lala as util
+
+
+if __name__ == "__main__":
+    
+    # util.calculate_dihedral_angle_gradient('ethane.xyz', 'ethane.log')
+    util.debug('ethane')  # Replace 'ethane' with the name of your file
+
